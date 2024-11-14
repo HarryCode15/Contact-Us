9b658b7f656bffec3f5056405903f7706ee91c27
HarryCode15 <harshbtekade137@gmail.com>
2024-11-15 02:42:14 +0530

    New project: Contact Us page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+
+
+const Navbar = ()=>{
+    return(
+        <nav>
+            <div className="logo">
+                <img src="/images/Support_logo.png" alt="Support Images" />
+            </div>
+            
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar;
